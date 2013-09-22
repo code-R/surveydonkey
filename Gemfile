70d592aac8b067ff4e44264e23f19a61047a1ad4
vamsi <vamsi@vamsi.local>
2013-09-22 08:18:29 -0500

    adding authorization for the application

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 ruby '1.9.3'
 
+gem 'cancan'
 gem 'devise'
 gem 'haml-rails'
 gem 'jquery-rails'
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,22 @@
 require 'spec_helper'
 
 describe User do
+  context "Accesible" do
+    subject { User.new }
+
+    accesible_attributes = %w(email password password_confirmation remember_me role)
+
+    accesible_attributes.each do |attr|
+      it { should be_accessible attr }
+    end
+
+    protected_attributes = User.new.attributes.keys - accesible_attributes
+
+    protected_attributes.each do |attr|
+      it { should_not be_accessible attr }
+    end
+  end
+
   it "should create a user with valid attributes" do
     FactoryGirl.create(:user)
   end
@@ -27,7 +43,7 @@ describe User do
     user.should_not be_valid
   end
 
-  describe "passwords" do
+  context "passwords" do
     let(:user) { user = FactoryGirl.build(:user) }
 
     it "should have a password attribute" do
@@ -39,7 +55,7 @@ describe User do
     end
   end
 
-  describe "password validations" do
+  context "password validations" do
 
     it "should require a password" do
       user = FactoryGirl.build(:user, password: '', password_confirmation: '')
@@ -55,6 +71,18 @@ describe User do
       short_password = 'abcd'
       user = FactoryGirl.build(:user,password: short_password, password_confirmation: short_password)
       user.should_not be_valid
+    end
+  end
+
+  context "#admin?" do
+    it "should be false when role is normal" do
+      user = FactoryGirl.create(:user)
+      user.should_not be_admin
+    end
+
+    it "should be false when role is normal" do
+      admin = FactoryGirl.create(:user, role: 'admin')
+      admin.should be_admin
     end
   end
 
