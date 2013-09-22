7a41f10c5c961eea5ac0c31f0ad522f22d94cf29
vamsi <vamsi@vamsi.local>
2013-09-22 01:54:23 -0500

    intializing project with basic rails code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SurveyDonkey::Application.config.secret_token = '69885b4fc4a3958faec74dce987c2afd7dc6adb1eb758ef3e6b0934f53ad254a1b69124b7312a7b9c4caed3f613ca39e79820ef361f0bd4995f1113ab040cf68'
